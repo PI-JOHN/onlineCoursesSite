51fa83aa8c7081f2a1c646209ac22e868e0cddbe
p_john <boriskashtanov80@gmail.com>
2020-06-21 12:42:24 +0400

    add Bootstrap

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/templates/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <title>PPH test site</title>
   </head>
   <body>
-    
+
   </body>
 </html>
